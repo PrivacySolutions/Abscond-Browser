22a33e8b72a1caec067d834ecc89a9d0ccb6a4e8
Mike Perry <mikeperry-git@torproject.org>
2012-12-04 16:03:13 -0800

    Block all plugins except flash.
    We cannot use the @mozilla.org/extensions/blocklist;1 service, because we
    actually want to stop plugins from ever entering the browser's process space
    and/or executing code (for example, AV plugins that collect statistics/analyse
    urls, magical toolbars that phone home or "help" the user, skype buttons that
    ruin our day, and censorship filters). Hence we rolled our own.
    
    See https://trac.torproject.org/projects/tor/ticket/3547#comment:6 for musings
    on a better way. Until then, it is delta-darwinism for us.

--- dom/plugins/base/nsPluginHost.cpp
+++ dom/plugins/base/nsPluginHost.cpp
@@ -1801,6 +1801,35 @@ struct CompareFilesByTime
 
 } // anonymous namespace
 
+PRBool nsPluginHost::GhettoBlacklist(nsIFile *pluginFile)
+{
+    nsCString leaf;
+    const char *leafStr;
+    nsresult rv;
+    
+    rv = pluginFile->GetNativeLeafName(leaf);
+    if (NS_FAILED(rv)) {
+        return PR_TRUE; // fuck 'em. blacklist.
+    }
+
+    leafStr = leaf.get();
+
+    if (!leafStr) {
+        return PR_TRUE; // fuck 'em. blacklist.
+    }
+
+    // libgnashplugin.so, libflashplayer.so, Flash Player-10.4-10.5.plugin,
+    // NPSWF32.dll, NPSWF64.dll
+    if (strstr(leafStr, "libgnashplugin") == leafStr ||
+        strstr(leafStr, "libflashplayer") == leafStr ||
+        strstr(leafStr, "Flash Player") == leafStr ||
+        strstr(leafStr, "NPSWF") == leafStr) {
+        return PR_FALSE;
+    }
+
+    return PR_TRUE; // fuck 'em. blacklist.
+}
+
 typedef NS_NPAPIPLUGIN_CALLBACK(char *, NP_GETMIMEDESCRIPTION)(void);
 
 nsresult nsPluginHost::ScanPluginsDirectory(nsIFile *pluginsDir,
@@ -1902,6 +1931,10 @@ nsresult nsPluginHost::ScanPluginsDirectory(nsIFile *pluginsDir,
     }
     if (isKnownInvalidPlugin) {
       continue;
+    }
+
+    if (GhettoBlacklist(localfile)) {
+        continue;
     }
 
     // if it is not found in cache info list or has been changed, create a new one
