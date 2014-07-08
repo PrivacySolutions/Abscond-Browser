32809b2f4949fbf32a7a13a56582249e624a059b
Mikal Villa <meeh@sigterm.no>
2014-07-08 23:41:33 +0200

    More branding fixes

--- browser/branding/official/pref/firefox-branding.js
+++ browser/branding/official/pref/firefox-branding.js
@@ -2,8 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-pref("startup.homepage_override_url", "");
-pref("startup.homepage_welcome_url", "http://sigterm.i2p/browser/%VERSION%/firstrun/");
+pref("startup.homepage_override_url", "about:home");
+pref("startup.homepage_welcome_url", "http://sigterm.i2p/browser/firstrun/%VERSION%");
 // Interval: Time between checks for a new version (in seconds)
 pref("app.update.interval", 43200); // 12 hours
 // The time interval between the downloading of mar file chunks in the
